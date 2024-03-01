8dd204019e1b2c00a52c828df7a619297abd5c5b
maxiadaro <maximiliano.adaro@capchase.com>
2024-03-01 18:56:08 -0300

    remove cypress split & add after:spec

--- cypress/commands/shared.ts
+++ cypress/commands/shared.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import cypressSplit from 'cypress-split';
+// import cypressSplit from 'cypress-split';
 import cypressFailFast from 'cypress-fail-fast/plugin';
 // eslint-disable-next-line no-restricted-imports
 import datadogPlugin from 'dd-trace/ci/cypress/plugin';
@@ -12,17 +12,17 @@ export const setupSharedNodeEvents = (
 ) => {
     datadogPlugin(on, config);
     cypressFailFast(on, config);
-    cypressSplit(on, config);
+    // cypressSplit(on, config);
     on('after:run', (results: CypressCommandLine.CypressRunResult) => {
         // console.log('CONSOLE: Cypress after:run', results);
         console.log('CONSOLE: Cypress after:run');
         return require('dd-trace/ci/cypress/after-run')(results)
     });
-    // on('after:spec', (_: Cypress.Spec, results: CypressCommandLine.RunResult) => {
-    // console.log('CONSOLE: Cypress after:spec', results);
-    // console.log('CONSOLE: Cypress after:spec');
-    // return removeAssetsIfNotFailedOrFlaky(results);
-    // });
+    on('after:spec', (_: Cypress.Spec, results: CypressCommandLine.RunResult) => {
+        // console.log('CONSOLE: Cypress after:spec', results);
+        console.log('CONSOLE: Cypress after:spec');
+        return removeAssetsIfNotFailedOrFlaky(results);
+    });
     on(
         'before:browser:launch',
         (browser: Cypress.Browser, launchOptions: Cypress.BrowserLaunchOptions) => {
