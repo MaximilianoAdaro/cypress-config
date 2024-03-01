eff59ec6392aaf73c42d2bd5e4776441cef6e998
maxiadaro <maximiliano.adaro@capchase.com>
2024-03-01 18:21:41 -0300

    update tests

--- cypress/src/e2e/integration/examples.spec.ts
+++ cypress/src/e2e/integration/examples.spec.ts
@@ -1,6 +1,6 @@
-describe('template spec', () => {
-  it('fails', () => {
-    cy.visit('https://example.cypress.io')
-    expect(true).to.equal(false)
-  })
-})
+// describe('template spec', () => {
+//   it('fails', () => {
+//     cy.visit('https://example.cypress.io')
+//     expect(true).to.equal(false)
+//   })
+// })
--- cypress/src/e2e/integration/test.spec.ts
+++ cypress/src/e2e/integration/test.spec.ts
@@ -1,5 +1,14 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+    it('1. passes', () => {
+        cy.visit('https://example.cypress.io')
+    })
+
+    it('2. fails', () => {
+        cy.visit('https://example.cypress.io')
+        expect(true).to.equal(false)
+    })
+
+    it('3. passes', () => {
+        cy.visit('https://example.cypress.io')
+    })
 })
