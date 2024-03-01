20c909c0650959377555e18bf8a309ee4bba4104
maxiadaro <maximiliano.adaro@capchase.com>
2024-03-01 18:35:07 -0300

    add cypress-fail-fast

--- cypress/commands/shared.ts
+++ cypress/commands/shared.ts
@@ -1,18 +1,16 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 // import cypressSplit from 'cypress-split';
-// import cypressFailFast from 'cypress-fail-fast/plugin';
+import cypressFailFast from 'cypress-fail-fast/plugin';
 // eslint-disable-next-line no-restricted-imports
 import datadogPlugin from 'dd-trace/ci/cypress/plugin';
-import {handleBrowserLaunchConfig, removeAssetsIfNotFailedOrFlaky} from "./assets";
 
 export const setupSharedNodeEvents = (
     on: any,
     config: any,
-    addCustomBrowserLaunchConfig?: (browser: any, launchOptions: any) => any
 ) => {
     datadogPlugin(on, config);
-    // cypressFailFast(on, config);
+    cypressFailFast(on, config);
     // cypressSplit(on, config);
     on('after:run', (results: CypressCommandLine.CypressRunResult) => {
         // console.log('CONSOLE: Cypress after run', results);
@@ -23,17 +21,19 @@ export const setupSharedNodeEvents = (
         ]);
     });
     // on('after:spec', (_: Cypress.Spec, results: CypressCommandLine.RunResult) => {
-        // console.log('CONSOLE: Cypress after spec', results);
-        // console.log('CONSOLE: Cypress after:spec');
-        // return Promise.allSettled([removeAssetsIfNotFailedOrFlaky(results)]);
+    // console.log('CONSOLE: Cypress after spec', results);
+    // console.log('CONSOLE: Cypress after:spec');
+    // return Promise.allSettled([removeAssetsIfNotFailedOrFlaky(results)]);
     // });
     on(
         'before:browser:launch',
         (browser: Cypress.Browser, launchOptions: Cypress.BrowserLaunchOptions) => {
-            // Some configs need to set extra args to the browser
-            if (addCustomBrowserLaunchConfig)
-                launchOptions = addCustomBrowserLaunchConfig(browser, launchOptions);
-            return handleBrowserLaunchConfig()(browser, launchOptions);
+            console.log(
+                "launching browser: '%s', is headless? '%s'",
+                browser.name,
+                browser.isHeadless,
+            );
+
         }
     );
 };
