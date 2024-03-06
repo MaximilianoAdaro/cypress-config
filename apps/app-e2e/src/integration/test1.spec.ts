f083718ed2d113d5727406d4218dc17cdb33d194
maxiadaro <maximiliano.adaro@capchase.com>
2024-03-06 00:32:40 -0300

    add afterEach errors

--- apps/app-e2e/src/integration/test1.spec.ts
+++ apps/app-e2e/src/integration/test1.spec.ts
@@ -1,4 +1,9 @@
 describe('test 1 template spec', () => {
+    afterEach(() => {
+        console.log('CONSOLE: afterEach test 1');
+        expect(true).to.equal(false)
+    })
+
     it('1. test passes', () => {
         expect(true).to.equal(true)
     })
--- apps/app-e2e/src/integration/test4.spec.ts
+++ apps/app-e2e/src/integration/test4.spec.ts
@@ -1,4 +1,9 @@
 describe('test 4 template spec', () => {
+    afterEach(() => {
+        console.log('CONSOLE: afterEach test 4');
+        expect(true).to.equal(false)
+    })
+
     it('1. test passes', () => {
         expect(true).to.equal(true)
     })
