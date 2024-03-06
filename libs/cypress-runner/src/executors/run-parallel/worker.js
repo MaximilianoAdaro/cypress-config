848404ff2484ff82c3d5aa7648f10f5d4240e911
maxiadaro <maximiliano.adaro@capchase.com>
2024-03-06 01:00:57 -0300

    Overrides

--- libs/cypress-runner/src/executors/run-parallel/worker.js
+++ libs/cypress-runner/src/executors/run-parallel/worker.js
@@ -2,8 +2,8 @@ const {parentPort} = require('worker_threads');
 const {runExecutor} = require('@nrwl/devkit');
 
 async function execute({targetDescription, overrides, context}) {
-    // console.log("Target Description: ", targetDescription);
-    // console.log("Overrides: ", overrides);
+    console.log("Target Description: ", targetDescription);
+    console.log("Overrides: ", overrides);
     // console.log("Context: ", context);
 
     for await (const output of await runExecutor(
